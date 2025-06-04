8c71fc534204ccce02aea7783814477410ce6f58
kev0037 <auk7495@gmail.com>
2025-06-04 18:04:58 +0100

    Add files via upload

---
+++ 21sleep.py
@@ -0,0 +1,20 @@
+from discord.ext import commands
+from discord import File
+
+bot = commands.Bot(command_prefix=".", self_bot=False)
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} is now running and active.")
+
+@bot.event
+async def on_message(message):
+    if message.author.name == "raydongg":
+        try:
+            await message.add_reaction("💀")
+        except Exception as e:
+            print(f"Couldn't react: {e}")
+
+
+
+bot.run("MTMwMjMzNDIwMjY4MjkzNzQ2OA.G-vjAG.AEeQ5hAqNdmbufvXwjzDOD28OaPlaR77EevZv8")
